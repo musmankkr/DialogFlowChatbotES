2e3a759d919db60be51da4c969824281c522ea01
usman.khokhar <usman.khokhar@ISBLT-9204>
2023-04-25 23:52:38 +0500

    Add project files.

---
+++ WebApi/Controllers/orderController.cs
@@ -0,0 +1,52 @@
+﻿using Google.Cloud.Dialogflow.V2;
+using Microsoft.AspNetCore.Mvc;
+using RestSharp;
+using Google.Protobuf;
+using System.Text;
+
+namespace WebApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class orderController : ControllerBase
+    {
+
+        private static readonly JsonParser jsonParser = new JsonParser(JsonParser.Settings.Default.WithIgnoreUnknownFields(true));
+
+        [HttpPost]
+        public async Task<JsonResult> Post()
+        {
+            WebhookRequest request;
+            using (var reader = new StreamReader(Request.Body))
+            {
+                request = jsonParser.Parse<WebhookRequest>(reader);
+            }
+
+            var pas = request.QueryResult.Parameters;
+            var orderId = pas.Fields["orderId"].StringValue;
+
+            var client = new RestClient("https://orderstatusapi-dot-organization-project-311520.uc.r.appspot.com/api");
+            var requestShipmentDate = new RestRequest("getOrderStatus/");
+            string jsonBody = "{\r\n \"orderId\": \"value\"\r\n}";
+            jsonBody = jsonBody.Replace("value", orderId);
+            requestShipmentDate.AddJsonBody("{\r\n \"orderId\": \"31312\"\r\n}");
+
+            var response = client.Post(requestShipmentDate).Content;
+            string dateString = response.Substring(response.IndexOf(":\"") + 2, 19);
+
+            DateTime shipmentDate = DateTime.Parse(dateString);
+            string formatedDate = shipmentDate.ToString("dddd, dd MMM yyyy");
+
+            var WebHookResponse = new WebhookResponse();
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Your order " + orderId + " will be shipped on "+formatedDate);
+
+            WebHookResponse.FulfillmentText = sb.ToString();
+
+            JsonResult jsonResult = new JsonResult(WebHookResponse);
+            return jsonResult;
+        }
+
+    }
+
+}
---
+++ WebApi/Program.cs
@@ -0,0 +1,48 @@
+using Google.Api;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.Configure<ForwardedHeadersOptions>(options =>
+{
+    options.ForwardedHeaders =
+        ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+});
+
+builder.Services.Configure<KestrelServerOptions>(options =>
+{
+    options.AllowSynchronousIO = true;
+});
+
+// If using IIS:
+builder.Services.Configure<IISServerOptions>(options =>
+{
+    options.AllowSynchronousIO = true;
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseForwardedHeaders();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
